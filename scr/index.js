8fef72e0437a633524eb4e3486c1a12fda0e8b6b
Karthikeyan.B <111240309+BManoj04@users.noreply.github.com>
2023-03-02 19:36:47 +0530

    2nd commit

--- scr/index.js
+++ scr/index.js
@@ -1,34 +1,53 @@
 const express = require("express")
 const ejsmate = require("ejs-mate")
-const {initializeApp} = require("firebase/app");
+const { initializeApp } = require("firebase/app");
 const path = require("path")
-const { getDatabase,onValue,ref} = require("firebase/database");
+const { getDatabase, onValue, ref } = require("firebase/database");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding")
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+
+
+const mapboxtoken = "pk.eyJ1Ijoia2FydGhpa2V5YW5iYWxhamkiLCJhIjoiY2xlbXZvajRsMDdnODN5bzh0dW94MDdsaSJ9.nAKCXeUn9ulAsNwit8IqmA"
+const geocoder = mbxGeocoding({ accessToken: mapboxtoken })
 
 const app = express()
 
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname, "public")))
 
-const firebaseConfig = {databaseURL: "https://locationtracker-62c3f-default-rtdb.firebaseio.com/",};
+const firebaseConfig = { databaseURL: "https://locationtracker-62c3f-default-rtdb.firebaseio.com/", };
 const app2 = initializeApp(firebaseConfig);
 const database = getDatabase(app2);
 
-app.engine("ejs",ejsmate)
-app.set("view engine","ejs")
+app.engine("ejs", ejsmate)
+app.set("view engine", "ejs")
 
-//< ------------------------------------------------------------------------------- >//
+// < ------------------------------------------------------------------------------- >
 
 const db = getDatabase();
-const d = ref(db,"users/");
+const d = ref(db, "users/");
 
-onValue(d,(snapshot) => {
+onValue(d, (snapshot) => {
     datas = snapshot.val()
     console.log(datas)
 })
 
-app.get("/",(req,res) => {
-    res.render("home.ejs",{datas})
+app.get("/", (req, res) => {
+    res.render("home.ejs", { datas })
 })
 
-app.listen(3000,() => {
+app.get("/:id", (req, res) => {
+    id = req.params["id"]
+    try {
+        lat = datas[id].lat
+        lon = datas[id].lon
+    } catch {
+        res.send("Can't Find Location")
+    }
+    res.render("map.ejs", { lat, lon })
+
+})
+
+app.listen(3000, () => {
 })
 
