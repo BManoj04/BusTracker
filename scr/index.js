09dd003a424f2e249ffabea97d257678d24f16ae
Karthikeyan.B <111240309+BManoj04@users.noreply.github.com>
2023-03-19 23:56:58 +0530

    3rd

--- scr/index.js
+++ scr/index.js
@@ -2,11 +2,11 @@ const express = require("express")
 const ejsmate = require("ejs-mate")
 const { initializeApp } = require("firebase/app");
 const path = require("path")
-const { getDatabase, onValue, ref } = require("firebase/database");
+const { getDatabase, onValue, ref, get } = require("firebase/database");
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding")
 var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
-
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2FydGhpa2V5YW5iYWxhamkiLCJhIjoiY2t2dGd3YmVnM2NrajJudGt1eHdscWVqMyJ9.A0jCxbqQ2SvjO8TVEz2Hxg';
 
 const mapboxtoken = "pk.eyJ1Ijoia2FydGhpa2V5YW5iYWxhamkiLCJhIjoiY2xlbXZvajRsMDdnODN5bzh0dW94MDdsaSJ9.nAKCXeUn9ulAsNwit8IqmA"
 const geocoder = mbxGeocoding({ accessToken: mapboxtoken })
@@ -14,6 +14,7 @@ const geocoder = mbxGeocoding({ accessToken: mapboxtoken })
 const app = express()
 
 app.use(express.static(path.join(__dirname, "public")))
+
 
 const firebaseConfig = { databaseURL: "https://locationtracker-62c3f-default-rtdb.firebaseio.com/", };
 const app2 = initializeApp(firebaseConfig);
@@ -23,6 +24,7 @@ app.engine("ejs", ejsmate)
 app.set("view engine", "ejs")
 
 // < ------------------------------------------------------------------------------- >
+
 
 const db = getDatabase();
 const d = ref(db, "users/");
@@ -38,14 +40,14 @@ app.get("/", (req, res) => {
 
 app.get("/:id", (req, res) => {
     id = req.params["id"]
-    try {
-        lat = datas[id].lat
-        lon = datas[id].lon
-    } catch {
-        res.send("Can't Find Location")
+    if(datas[id]){
+    lat = datas[id].lat
+    lon = datas[id].lon
+    console.log(lat,lon)
+    res.render("map.ejs", { lat, lon, d, onValue, id,mapboxgl,get})
+    }else{
+        res.send("NO DATA")
     }
-    res.render("map.ejs", { lat, lon })
-
 })
 
 app.listen(3000, () => {
---
+++ scr/public/map.js
@@ -0,0 +1,33 @@
+
+// //const { initializeApp } = require("firebase/app");
+// import { initializeApp } from "firebase/app";
+
+// const { getDatabase, onValue, ref, get } = require("firebase/database");
+
+
+// mapboxgl.accessToken = 'pk.eyJ1Ijoia2FydGhpa2V5YW5iYWxhamkiLCJhIjoiY2t2dGd3YmVnM2NrajJudGt1eHdscWVqMyJ9.A0jCxbqQ2SvjO8TVEz2Hxg';
+//         const map = new mapboxgl.Map({
+//             container: 'map', // container ID
+//             style: 'mapbox://styles/mapbox/streets-v12', // style URL
+            
+//             center: [longi2, lati2], // starting position [lng, lat]
+//             zoom: 15, // starting zoom
+//         });
+        
+//         const marker = new mapboxgl.Marker()
+//                 .setLngLat([longi2, lati2])
+//                 .addTo(map);
+
+//         setInterval(function(){ 
+//         lati2 = parseFloat("<%-lati%>")
+//         longi2 = parseFloat("<%-longi%>")
+//         console.log("...............int.............")
+//         console.log(longi2,lati2)
+//         console.log(".............../int.............")
+//         marker.setLngLat([longi2,lati2])
+//         marker.addTo(map)
+//             console.log(longi2,lati2)
+//         },2500)
+            
+                
+               
