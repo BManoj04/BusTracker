e37cc31dcd38421de53063082306061a18c2cfa3
Karthikeyan.B <59909122+karthikeyan2002@users.noreply.github.com>
2023-04-05 23:47:30 +0530

    navbar updation

--- scr/public/allpg.js
+++ scr/public/allpg.js
@@ -1,36 +1,37 @@
 const navSlide = () => {
+
   const burger = document.querySelector('.burger');
+  console.log(burger)
   const nav = document.querySelector('.nav-links');
   const navLinks = document.querySelectorAll('.nav-links li');
-
-  // On scroll 
-  $(window).on('scroll', function () {
-    if ($(window).scrollTop()) {
-      $('nav').addClass('nav-opacity');
-    }
-    else {
-      $('nav').removeClass('nav-opacity');
-    }
-  });
-
-  // Toggle Nav
-  burger.addEventListener('click', () => {
+    
+     // On scroll 
+  $(window).on('scroll', function() {
+  if ($(window).scrollTop()) {
+    $('nav').addClass('nav-opacity');
+  }
+  else { 
+    $('nav').removeClass('nav-opacity');
+  }
+});
+  
+    // Toggle Nav
+  burger.addEventListener('click', () =>{
     nav.classList.toggle('nav-activ');
-
+   
     // Anime Links
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = '';
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .3}s`;
-      }
-    });
-
-    // Burger Animation
-    burger.classList.toggle('toggle')
-
+  navLinks.forEach((link, index) => {
+    if(link.style.animation){
+      link.style.animation = '';
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .3}s`;
+    }
   });
-
+    
+    // Burger Animation
+    burger.classList.toggle('toggle') 
+});
+  
 }
 
 navSlide();
